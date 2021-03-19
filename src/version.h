d5d7c6262f5901dd59ca827a3e7c39b5d017351e
ldir92664 <ldir92664@163.com>
2021-03-19 14:08:28 +0800

    Update version.h

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-/* XMRig
+/* BeHash
  * Copyright 2010      Jeff Garzik <jgarzik@pobox.com>
  * Copyright 2012-2014 pooler      <pooler@litecoinpool.org>
  * Copyright 2014      Lucas Jones <https://github.com/lucasjones>
@@ -6,7 +6,7 @@
  * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
  * Copyright 2017-2018 XMR-Stak    <https://github.com/fireice-uk>, <https://github.com/psychocrypt>
  * Copyright 2018-2021 SChernykh   <https://github.com/SChernykh>
- * Copyright 2016-2021 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+ * Copyright 2016-2021 BeHash       <https://github.com/BeHash>, <support@BeHash.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -22,16 +22,16 @@
  *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef XMRIG_VERSION_H
-#define XMRIG_VERSION_H
+#ifndef BeHash_VERSION_H
+#define BeHash_VERSION_H
 
-#define APP_ID        "xmrig"
-#define APP_NAME      "XMRig"
-#define APP_DESC      "XMRig miner"
+#define APP_ID        "BeHash"
+#define APP_NAME      "BeHash"
+#define APP_DESC      "BeHash Worker"
 #define APP_VERSION   "6.10.0"
-#define APP_DOMAIN    "xmrig.com"
-#define APP_SITE      "www.xmrig.com"
-#define APP_COPYRIGHT "Copyright (C) 2016-2021 xmrig.com"
+#define APP_DOMAIN    "BeHash.com"
+#define APP_SITE      "www.BeHash.com"
+#define APP_COPYRIGHT "Copyright (C) 2016-2021 BeHash.com"
 #define APP_KIND      "miner"
 
 #define APP_VER_MAJOR  6
@@ -56,4 +56,4 @@
 #   endif
 #endif
 
-#endif /* XMRIG_VERSION_H */
+#endif /* BeHash_VERSION_H */
